352739788d230502d1ae944310458db34ab7826c
Ryan Domingue <ryan@alley.co>
2023-08-25 15:58:16 -0700

    Removes block styles

--- blocks/query-link/edit.tsx
+++ blocks/query-link/edit.tsx
@@ -1,8 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 
-import './index.scss';
-
 /**
  * The wp-curate/query-link block edit function.
  *
