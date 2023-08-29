dc6f62661b63f93fee96a812ba3aaff32bd78054
Julio Brana <julio@alley.co>
2023-08-29 10:43:37 +0200

    Update blocks/is-false/index.tsx
    Co-authored-by: Ryan Domingue <goodguyry@users.noreply.github.com>

--- blocks/is-false/index.tsx
+++ blocks/is-false/index.tsx
@@ -4,8 +4,6 @@ import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 import edit from './edit';
 import metadata from './block.json';
 
-import './style.scss';
-
 /* @ts-expect-error Provided types are inaccurate to the actual plugin API. */
 registerBlockType(metadata, {
   edit,
