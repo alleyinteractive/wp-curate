49d2999dc29eea12be74027ae88157969ae33208
Julio Braña Bouza <julio@alley.co>
2023-08-30 22:14:20 +0300

    add help to fields

--- blocks/condition/edit.tsx
+++ blocks/condition/edit.tsx
@@ -1,6 +1,8 @@
 import { __ } from '@wordpress/i18n';
 import { InnerBlocks, InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import { PanelRow, SelectControl, TextControl } from '@wordpress/components';
+import {
+  PanelBody, PanelRow, SelectControl, TextControl,
+} from '@wordpress/components';
 
 interface EditProps {
   attributes: {
@@ -40,71 +42,90 @@ export default function Edit({
 
       <InspectorControls>
         { /* @ts-ignore */ }
-        <PanelRow>
+        <PanelBody
+          title={__('Condition', 'the-wrap')}
+          initialOpen
+        >
           { /* @ts-ignore */ }
-          <TextControl
-            label={__('Query', 'wp-curate')}
-            onChange={(next) => setAttributes({ query: next })}
-            value={query}
-          />
-        </PanelRow>
+          <PanelRow>
+            { /* @ts-ignore */ }
+            <TextControl
+              label={__('Query', 'wp-curate')}
+              help={__('Query condition, ie "is_home" or "is_category"', 'wp-curate')}
+              onChange={(next) => setAttributes({ query: next })}
+              value={query}
+            />
+          </PanelRow>
+
+          { /* @ts-ignore */ }
+          <PanelRow>
+            { /* @ts-ignore */ }
+            <TextControl
+              label={__('Post', 'wp-curate')}
+              help={__('Post condition, ie "is_content"', 'wp-curate')}
+              onChange={(next) => setAttributes({ post: next })}
+              value={post}
+            />
+          </PanelRow>
+
+          { /* @ts-ignore */ }
+          <PanelRow>
+            { /* @ts-ignore */ }
+            <TextControl
+              label={__('Custom', 'wp-curate')}
+              help={__('Custom condition, ie "is_column"', 'wp-curate')}
+              onChange={(next) => setAttributes({ custom: next })}
+              value={custom}
+            />
+          </PanelRow>
+
+          { /* @ts-ignore */ }
+          <PanelRow>
+            { /* @ts-ignore */ }
+            <TextControl
+              label={__('Condition', 'wp-curate')}
+              help={__('Any other condition', 'wp-curate')}
+              onChange={(next) => setAttributes({ condition: next })}
+              value={condition}
+            />
+          </PanelRow>
+        </PanelBody>
 
         { /* @ts-ignore */ }
-        <PanelRow>
+        <PanelBody
+          title={__('Index Condition', 'the-wrap')}
+        >
+          <p>{__('Checks the index of how many times the parent condition block has been rendered, ie "Equals to 0", "Greater than 5"', 'wp-curate')}</p>
+
           { /* @ts-ignore */ }
-          <TextControl
-            label={__('Post', 'wp-curate')}
-            onChange={(next) => setAttributes({ post: next })}
-            value={post}
-          />
-        </PanelRow>
+          <PanelRow>
+            <SelectControl
+              label={__('Index Operator', 'wp-curate')}
+              value={operator}
+              options={[
+                { value: '', label: __('Select Operator', 'wp-curate') },
+                { value: '===', label: __('Equal', 'wp-curate') },
+                { value: '!==', label: __('Not equal', 'wp-curate') },
+                { value: '>', label: __('Greater than', 'wp-curate') },
+                { value: '<', label: __('Less than', 'wp-curate') },
+                { value: '>=', label: __('Greater than or equal to', 'wp-curate') },
+                { value: '<=', label: __('Less than or equal to', 'wp-curate') },
+              ]}
+              onChange={(next: string) => setAttributes({ index: { [next]: compared } })}
+            />
+          </PanelRow>
 
-        { /* @ts-ignore */ }
-        <PanelRow>
           { /* @ts-ignore */ }
-          <TextControl
-            label={__('Custom', 'wp-curate')}
-            onChange={(next) => setAttributes({ custom: next })}
-            value={custom}
-          />
-        </PanelRow>
-
-        { /* @ts-ignore */ }
-        <PanelRow>
-          { /* @ts-ignore */ }
-          <TextControl
-            label={__('Condition', 'wp-curate')}
-            onChange={(next) => setAttributes({ condition: next })}
-            value={condition}
-          />
-        </PanelRow>
-
-        <SelectControl
-          label={__('Index Operator', 'wp-curate')}
-          value={operator}
-          options={[
-            { value: '', label: __('Select Operator', 'wp-curate') },
-            { value: '===', label: __('Equal', 'wp-curate') },
-            { value: '!==', label: __('Not equal', 'wp-curate') },
-            { value: '>', label: __('Greater than', 'wp-curate') },
-            { value: '<', label: __('Less than', 'wp-curate') },
-            { value: '>=', label: __('Greater than or equal to', 'wp-curate') },
-            { value: '<=', label: __('Less than or equal to', 'wp-curate') },
-          ]}
-          onChange={(next: string) => setAttributes({ index: { [next]: compared } })}
-        />
-
-        { /* @ts-ignore */ }
-        <PanelRow>
-          { /* @ts-ignore */ }
-          <TextControl
-            label={__('Index compared', 'wp-curate')}
-            onChange={(next) => setAttributes({ index: { [operator]: next } })}
-            type="number"
-            value={compared}
-          />
-        </PanelRow>
-
+          <PanelRow>
+            { /* @ts-ignore */ }
+            <TextControl
+              label={__('Index compared', 'wp-curate')}
+              onChange={(next) => setAttributes({ index: { [operator]: next } })}
+              type="number"
+              value={compared}
+            />
+          </PanelRow>
+        </PanelBody>
       </InspectorControls>
     </>
   );
