e3e7d56e3eb172c89652f862bfc42af274f6b6a3
Greg Marshall <greg@alley.co>
2024-02-14 19:09:15 -0600

    clean it up

--- blocks/query/edit.tsx
+++ blocks/query/edit.tsx
@@ -202,8 +202,7 @@ export default function Edit({
     const newManualPosts = [...manualPosts];
     // If the post is already in the list, remove it.
     if (newManualPosts.includes(id)) {
-      const existing = newManualPosts.indexOf(id);
-      newManualPosts.splice(existing, 1, null);
+      newManualPosts.splice(newManualPosts.indexOf(id), 1, null);
     }
     newManualPosts.splice(index, 1, id);
     setAttributes({ posts: newManualPosts });
