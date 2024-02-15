cee22bc24d6fbea7d501c89fce9d9146eacf0fa3
mogmarsh <mogmarsh@users.noreply.github.com>
2024-02-15 14:26:09 +0000

    Built changes for v1.6.3 from develop 429c4dc2c6ec3a347ef415256f4c0a3ad66bc7dc

---
+++ build/post/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f69b36178d6ef3950a04');
---
+++ build/query/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '722beeab2f47d793264f');
