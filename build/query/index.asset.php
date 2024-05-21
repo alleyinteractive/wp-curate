06d6899dd711e39f8507b90a5840587f7068ace8
dlh01 <dlh01@users.noreply.github.com>
2024-05-21 17:17:04 +0000

    Built changes for v1.10.0 from develop 710b2177d54d6ee6aa65abf7647c3d74288a2162

---
+++ build/query/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1cff9ac17495a5de798f');
