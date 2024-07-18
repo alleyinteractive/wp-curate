aea27e55caa12d2ab7a5352ed904820b7de0929f
efuller <eric@alley.co>
2024-07-18 12:24:44 -0400

    Test that unique pinned posts are not enforced if not enabled via post meta

--- tests/feature/class-unique-pinned-post-test.php
+++ tests/feature/class-unique-pinned-post-test.php
@@ -43,17 +43,8 @@ class Unique_Pinned_Post_Test extends Test_Case {
 		]);
 	}
 
-	/**
-	 *  If configured, pinned posts should only display once regardless of where on the page
-	 *  the post was pinned at.
-	 */
-	public function test_pinned_post_only_displays_once() {
-		$pinned_post = static::factory()->post->as_models()->create_and_get([
-			'post_title' => 'PinnedPost 1',
-		]);
-		$pinned_post_id = $pinned_post->id();
-
-		$content = <<<HTML
+	private function create_html_content( $pinned_post_id ) {
+		return <<<HTML
 				<!-- wp:wp-curate/query {"numberOfPosts":2,"postTypes":["post"],"posts":[null,null]} -->
 				<div class="wp-block-wp-curate-query"><!-- wp:post-template -->
 				<!-- wp:wp-curate/post -->
@@ -78,6 +69,50 @@ class Unique_Pinned_Post_Test extends Test_Case {
 				<!-- /wp:post-template --></div>
 				<!-- /wp:wp-curate/query -->
 				HTML;
+	}
+
+	/**
+	 * Do not enforce unique pinned posts if it is not enabled.
+	 */
+	public function test_do_not_enforce_unique_pinned_posts_if_not_enabled() {
+		$pinned_post = static::factory()->post->as_models()->create_and_get([
+			'post_title' => 'PinnedPost 1',
+		]);
+		$pinned_post_id = $pinned_post->id();
+
+		$content = $this->create_html_content( $pinned_post_id );
+
+		$post = static::factory()
+			->post
+			->with_meta(
+				[
+					'wp_curate_deduplication'       => '1',
+					'wp_curate_unique_pinned_posts' => '0'
+				]
+			)
+			->as_models()
+			->create_and_get(
+				[
+					'post_content' => print_r($content, true ),
+				]);
+
+		$page = $this->get( $post )->assertOk();
+
+		$occurrences = substr_count( $page->get_content(), 'PinnedPost 1' );
+		$this->assertEquals( 2, $occurrences );
+	}
+
+	/**
+	 *  If configured, pinned posts should only display once regardless of where on the page
+	 *  the post was pinned at.
+	 */
+	public function test_pinned_post_only_displays_once() {
+		$pinned_post = static::factory()->post->as_models()->create_and_get([
+			'post_title' => 'PinnedPost 1',
+		]);
+		$pinned_post_id = $pinned_post->id();
+
+		$content = $this->create_html_content( $pinned_post_id );
 
 		$post = static::factory()
 			->post
@@ -94,7 +129,6 @@ class Unique_Pinned_Post_Test extends Test_Case {
 				]);
 
 		$page = $this->get( $post )->assertOk();
-		dump($page->get_content());
 
 		$occurrences = substr_count( $page->get_content(), 'PinnedPost 1' );
 		$this->assertEquals( 1, $occurrences );
