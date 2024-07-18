75c900c7aa8b71cc39585422639a1cdbe0afa098
mogmarsh <mogmarsh@users.noreply.github.com>
2024-07-18 15:36:43 +0000

    Built changes for v2.0.1 from develop 8f00bea8948f7f6ea164866d4404fa54ca5d047a

---
+++ build/query/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'bbb8ed407a182b5361ae');
---
+++ build/slotfills/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => 'c391ba6f34c852170897');
