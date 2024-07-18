ff76a20987a41bc6229a503f9f0a3fbf28bb8041
efuller <eric@alley.co>
2024-07-18 14:58:46 -0400

    Add sqlite back in

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,7 @@
  */
 \Mantle\Testing\manager()
 	->maybe_rsync_plugin()
+	->with_sqlite()
 	// Load the main file of the plugin.
 	->loaded( fn () => require_once __DIR__ . '/../wp-curate.php' )
 	->install();
