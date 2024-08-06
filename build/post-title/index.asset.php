863131554aef962a646b5f481484a9871c40ced5
ellm <ellm@users.noreply.github.com>
2024-08-06 15:42:59 +0000

    Built changes for v2.2.0 from develop 498e0be635388b633fa2d34d41368b34884a8bc8

---
+++ build/post-title/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => '214d5c2d723ada84732c');
---
+++ build/slotfills/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '6cabeea1c2c317a4b3ae');
