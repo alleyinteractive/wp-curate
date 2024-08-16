b49a488ea637e6c2cd5789e7f7cbc1d814659339
efuller <eric@alley.co>
2024-08-16 16:23:26 -0400

    Sure up types and formatting

--- services/queryBlockPostFetcher/index.ts
+++ services/queryBlockPostFetcher/index.ts
@@ -2,7 +2,11 @@ import apiFetch from '@wordpress/api-fetch';
 
 type FetcherProps = [string | undefined, number];
 
-const queryBlockPostFetcher = ([url, currentPostId]: FetcherProps) => apiFetch({ path: url })
+type PostResponse = number[] | { id: number };
+
+const queryBlockPostFetcher = (
+  [url, currentPostId]: FetcherProps,
+) => apiFetch<PostResponse>({ path: url })
   .then((response) => {
     let revisedResponse;
     // If the response is an array, filter out the current post.
