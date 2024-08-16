16a8575c864c308f8bc7237181d4411ea53531b1
efuller <eric@alley.co>
2024-08-16 16:11:01 -0400

    Build posts API path query args

---
+++ services/buildPostsApiPath/index.ts
@@ -0,0 +1,23 @@
+import { addQueryArgs } from '@wordpress/url';
+
+interface PathProps {
+  search: string,
+  offset: number,
+  postType: string,
+  status: 'publish',
+  perPage: 20,
+  orderBy: string,
+  currentPostId: number
+}
+
+export default function buildPostsApiPath(pathProps: PathProps) {
+  return addQueryArgs('/wp-curate/v1/posts', {
+    search: pathProps.search,
+    offset: pathProps.offset,
+    post_type: pathProps.postType,
+    status: pathProps.status,
+    per_page: pathProps.perPage,
+    orderby: pathProps.orderBy,
+    current_post_id: pathProps.currentPostId,
+  });
+}
