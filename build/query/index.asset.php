c4a99b7474da087e7ec5a8647fccb70dfebd9f31
samhermes <samhermes@users.noreply.github.com>
2024-09-09 20:16:36 +0000

    Built changes for v2.3.2 from develop ce3d00d5c82c58afbd57a9f54572652f843328df

---
+++ build/query/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'bbc7b3412e4b948789db');
