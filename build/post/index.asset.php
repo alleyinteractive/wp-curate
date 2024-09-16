858910780c81edb6e5620bb8fac0919418795d72
mogmarsh <mogmarsh@users.noreply.github.com>
2024-09-16 21:59:03 +0000

    Built changes for v2.3.3 from develop dcf9298077f22ee55b337fad7428defce40babde

---
+++ build/post/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '851872d869f6a2bd0391');
---
+++ build/slotfills/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '16447fc48d46bfbccce3');
