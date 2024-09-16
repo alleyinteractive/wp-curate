697d81c618a74c9f3a714d890fd4d1c87ec12aff
Greg Marshall <greg@alley.co>
2024-09-16 12:59:06 -0500

    make sure there is an id

--- src/class-plugin-curated-posts.php
+++ src/class-plugin-curated-posts.php
@@ -75,6 +75,9 @@ final class Plugin_Curated_Posts implements Curated_Posts {
 
 		$pinned_posts = $attributes['posts'] ?? $block_type->attributes['posts']['default'];
 		$pinned_posts = array_map( function ( $id ) {
+			if ( ! $id ) {
+				return null;
+			}
 			return 'publish' === get_post_status( $id ) ? $id : null;
 		}, $pinned_posts );
 
