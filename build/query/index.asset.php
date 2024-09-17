3a48afabd0e0202bb6e4588f2d5e566fde026e49
mogmarsh <mogmarsh@users.noreply.github.com>
2024-09-17 16:17:44 +0000

    Built changes for v2.4.0 from develop c5052b3114bec071330b2a0d45d1bbad1aefda5e

---
+++ build/query/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'af4e1aa0dfb782c3b14f');
---
+++ build/subquery/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2772c508d7a9dc97172f');
