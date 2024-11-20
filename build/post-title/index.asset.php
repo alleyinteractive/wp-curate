02103d2d66ccb3c4758f510ea9441087a7be3ca2
mogmarsh <mogmarsh@users.noreply.github.com>
2024-11-20 14:37:11 +0000

    Built changes for v2.4.3 from develop 19dfe4acf5ddf966cf1b2443ad1cc80bd9cba9c8

---
+++ build/post-title/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n', 'wp-primitives'), 'version' => 'f0eaf1451bbb264879a4');
---
+++ build/query/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1c8c87a8e0e41a445f3f');
