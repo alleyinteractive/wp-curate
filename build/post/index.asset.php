dad84ecd4c73d79df8f299801a63499c8fd99d15
mogmarsh <mogmarsh@users.noreply.github.com>
2025-10-15 20:36:19 +0000

    Built changes for v2.7.2 from develop c384cd2dfb039855804df28bdb99d6a1931fb573

---
+++ build/post/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '664d4aea121adf70a938');
---
+++ build/slotfills/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '30b6ec7ffa48443d2860');
