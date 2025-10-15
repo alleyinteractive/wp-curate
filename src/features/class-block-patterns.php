33cdb0a8475939272a88df4b9183e27e8f8a216a
Greg Marshall <greg@alley.co>
2025-10-15 16:18:31 -0500

    added two real layouts

--- src/features/class-block-patterns.php
+++ src/features/class-block-patterns.php
@@ -27,20 +27,20 @@ final class Block_Patterns implements Feature {
 		register_block_pattern(
 			'wp-curate/query-list',
 			[
-				'title'       => __( 'WP Curate Query: List', 'wp-curate' ),
-				'description' => _x( 'A list style query block.', 'Block pattern description', 'wp-curate' ),
-				'content'     => '<!-- wp:wp-curate/query {"postTypes":["post"]} --><div class="wp-block-wp-curate-query"><!-- wp:post-template --><!-- wp:wp-curate/post --><div class="wp-block-wp-curate-post"><!-- wp:post-featured-image /--><!-- wp:post-title {"isLink":true} /--></div><!-- /wp:wp-curate/post --><!-- /wp:post-template --></div><!-- /wp:wp-curate/query -->',
+				'title'       => __( 'WP Curate Query: 3up', 'wp-curate' ),
+				'description' => __( 'A 3up query block.', 'wp-curate' ),
+				'content'     => '<!-- wp:wp-curate/query {"numberOfPosts":3,"postTypes":["post"],"className":"threeup"} --><div class="wp-block-wp-curate-query threeup"><!-- wp:post-template --><!-- wp:wp-curate/post --><div class="wp-block-wp-curate-post"><!-- wp:post-featured-image /--><!-- wp:post-title {"isLink":true,"fontSize":"large"} /--></div><!-- /wp:wp-curate/post --><!-- /wp:post-template --></div><!-- /wp:wp-curate/query -->',
 				'categories'  => [ 'curate' ],
-				'keywords'    => [ 'curate', 'query', 'list' ],
+				'keywords'    => [ 'curate', 'query', 'threeup' ],
 				'blockTypes'  => [ 'wp-curate/query' ],
 			],
 		);
 		register_block_pattern(
 			'wp-curate/query-list-2',
 			[
-				'title'       => __( 'WP Curate Query: List 2', 'wp-curate' ),
-				'description' => _x( 'A list style query block.', 'Block pattern description', 'wp-curate' ),
-				'content'     => '<!-- wp:wp-curate/query {"postTypes":["post"]} --><div class="wp-block-wp-curate-query"><!-- wp:post-template --><!-- wp:wp-curate/post --><div class="wp-block-wp-curate-post"><!-- wp:post-title {"isLink":true} /--><!-- wp:post-featured-image /--><!-- wp:post-excerpt /--></div><!-- /wp:wp-curate/post --><!-- /wp:post-template --></div><!-- /wp:wp-curate/query -->',
+				'title'       => __( 'WP Curate Query: 1 x 2', 'wp-curate' ),
+				'description' => __( 'A query block with one large post and two smaller posts.', 'wp-curate' ),
+				'content'     => '<!-- wp:wp-curate/query {"numberOfPosts":3,"postTypes":["post"],"className":"onextwo"} --><div class="wp-block-wp-curate-query onextwo"><!-- wp:post-template --><!-- wp:wp-curate/post --><div class="wp-block-wp-curate-post"><!-- wp:post-featured-image /--><!-- wp:post-title {"isLink":true,"fontSize":"large"} /--></div><!-- /wp:wp-curate/post --><!-- /wp:post-template --></div><!-- /wp:wp-curate/query -->',
 				'categories'  => [ 'curate' ],
 				'keywords'    => [ 'curate', 'query', 'list' ],
 				'blockTypes'  => [ 'wp-curate/query' ],
