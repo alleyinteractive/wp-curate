0e1da486d95718b49712e2b9403b7232cf524d2a
Ron <ron@alley.com>
2026-04-09 14:20:07 -0700

    FAM-3332: Adds View and Edit post buttons to the toolbar.

--- blocks/post/edit.tsx
+++ blocks/post/edit.tsx
@@ -3,12 +3,13 @@ import classnames from 'classnames';
 import type { WP_REST_API_Post as WpRestApiPost } from 'wp-types'; // eslint-disable-line camelcase
 
 // @ts-expect-error BlockContextProvider not available in types yet.
-import { InnerBlocks, useBlockProps, BlockContextProvider } from '@wordpress/block-editor';
+import { BlockContextProvider, BlockControls, InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 import { PostPicker, usePostById } from '@alleyinteractive/block-editor-tools';
 import { dispatch, select, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import { Button, Notice } from '@wordpress/components';
+import { Button, Notice, ToolbarGroup, ToolbarButton } from '@wordpress/components';
 import { useCallback } from '@wordpress/element';
+import { pencil, seen } from '@wordpress/icons';
 
 import type { Block } from '../../types/block';
 import NoRender from './norender';
@@ -308,6 +309,25 @@ export default function Edit({
         },
       )}
     >
+      <BlockControls>
+        <ToolbarGroup>
+          <ToolbarButton
+            icon={pencil}
+            label={__('Edit post in a new tab', 'wp-curate')}
+            onClick={() => {
+              window.open(`/wp-admin/post.php?post=${postId}&action=edit`, '_blank');
+            }}
+          />
+          <ToolbarButton
+            icon={seen}
+            label={__('View Post in a new tab', 'wp-curate')}
+            onClick={() => {
+              window.open(postObj?.link, '_blank');
+            }}
+          />
+        </ToolbarGroup>
+      </BlockControls>
+
       {typeof postObj === 'object' && postObj !== null && 'status' in postObj && postObj.status === 'future' ? (
         <Notice
           status="warning"
