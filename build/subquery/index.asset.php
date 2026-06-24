78d2e6b2d6d5ffcb714fd55efaa324948a55050d
jessicamgoddard <jessicamgoddard@users.noreply.github.com>
2026-06-24 14:25:04 +0000

    Built changes for v3.2.1 from develop a54f618f8b6acc9a283d4add0503dc4ceeee61df

---
+++ build/subquery/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => 'da22ec49988515be27e1');
