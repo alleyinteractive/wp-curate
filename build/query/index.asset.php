394d28c1414ea1102418c700ea4da826a22988f5
kaitlinbolling <kaitlinbolling@users.noreply.github.com>
2026-06-25 14:22:43 +0000

    Built changes for v3.2.2 from develop 03b2753e1e9298fdb548b659bcc689d9ab1ecb21

---
+++ build/query/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => 'd2fe27c68a91e8eebcb4');
